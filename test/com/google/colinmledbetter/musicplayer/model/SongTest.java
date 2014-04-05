1811746991b5dedc42c6b0016a583a42ff399cd6
Colin Ledbetter <colinmledbetter@gmail.com>
2014-04-05 11:04:52 -0400

    Implemented getSongTitle into Song

--- test/com/google/colinmledbetter/musicplayer/model/SongTest.java
+++ test/com/google/colinmledbetter/musicplayer/model/SongTest.java
@@ -2,13 +2,20 @@ package com.google.colinmledbetter.musicplayer.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
 
 public class SongTest {
 	
 	private Song song;
 	
+	@Before
+	public void loadSong() {
+		song = new Song("test-assets/Phlank_Document_1.mp3");
+	}
+	
+	@Test
 	public void testSongTitle() {
-		Assert.assertEquals("Document 1", song.getTitle());
+		Assert.assertEquals("Document 1", song.getSongTitle());
 	}
 
 }
