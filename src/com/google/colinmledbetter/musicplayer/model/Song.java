943686873830833f7e0f6d65b5f0559630157c18
Colin Ledbetter <colinmledbetter@gmail.com>
2014-04-05 11:05:18 -0400

    Switched to non-javadoc jaudiotagger library

---
+++ src/com/google/colinmledbetter/musicplayer/model/Song.java
@@ -0,0 +1,27 @@
+package com.google.colinmledbetter.musicplayer.model;
+
+import java.io.File;
+
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+
+public class Song {
+	
+	private Tag tag;
+	
+	public Song(String filepath) {
+		try {
+			AudioFile file = AudioFileIO.read(new File(filepath));
+			tag = file.getTag();
+		} catch (Exception e) {
+//			e.printStackTrace();
+		}
+	}
+	
+	public String getSongTitle() {
+		return tag.getFirst(FieldKey.TITLE);
+	}
+
+}
